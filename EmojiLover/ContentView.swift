ee12c9e66fe62ad0392213428107d177eff32416
Lakshya Goel <lgoel340@gmail.com>
2023-08-27 13:06:38 +0530

    Emoji App

---
+++ EmojiLover/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  EmojiLover
+//
+//  Created by Lakshya Goel on 27/08/23.
+//
+
+import SwiftUI
+
+enum Emoji: String, CaseIterable {
+    case 😂,😣,🇮🇳,🥳,🥰,😫,😱
+}
+
+struct ContentView: View {
+    @State var selection: Emoji = .😂
+    
+    var body: some View {
+        NavigationView {
+            VStack{
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                
+                Picker("Select Emoji", selection: $selection){
+                    ForEach(Emoji.allCases, id: \.self) { emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Lovers!")
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
